30a51fc781e46ac1d7539ec23509a3adbc1f2cd1
tmadile@gmail.com <fake@1234>
2021-06-11 08:51:44 +0200

    added class diagram and readme file

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # ThamangMadile
+
+## How it can be run
+
+1. Clone repository @ https://github.com/tedmad/ThamangMadile.git
+2. Import the repository into InteliJ
+3. Navigate to src>main>java>com>example>restservice and open RestServiceApplication.java
+4. Run the class 
+5. Go to web browser and go this link http://localhost:8080/swagger-ui/
+6. Click on the invoice-controller link
+7. Click get button
+8. Click try-it-out button
+9. Fill the fields:
+    bill: 100.0
+    billType: 0
+    user: 1
+   
+10. Click execute button
+11. You should get the following response
+    {
+    "id": 1,
+    "amountPaid": 100,
+    "discount": 35,
+    "netPayable": 65
+    }
+    
+
+## Code Coverage
+1. Right click "rest-service" on the project explorer
+2. Navigate to "Run 'All Test' with Coverage
+3. The coverage explorer should appear on the right, then navigate to com>example>restservice 
+
+    
--- src/main/java/com/example/restservice/InvoiceController.java
+++ src/main/java/com/example/restservice/InvoiceController.java
@@ -1,5 +1,10 @@
 package com.example.restservice;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +25,21 @@ public class InvoiceController {
     @Autowired
     private DiscountService discountService;
 
+
+
     /**
-     * This a handler method for HTTP GET request.
+     * Get invoice based on bill, billType and user.
      * @param bill The total amount of goods to be purchased.
      * @param billType  The type of goods to be purchased. Value 0 indicates non-grocery.
      * @param user  The type of user. Value 1 = Employee, 2 = Affiliate and 3 = Long Time Customer.
      * @return Invoice This returns Invoice object with invoice ID, bill and net payable amount.
      */
+    @Operation(summary = "Get invoice based on bill, billType and user", description = "returns Invoice object with invoice ID, bill and net payable amount")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful GET request",
+                    content = @Content(schema = @Schema(implementation = Invoice.class))),
+            @ApiResponse(responseCode = "404", description = "Invoice not found"),
+            @ApiResponse(responseCode = "404", description = "Invoice not found")})
     @GetMapping("/invoice")
     public Invoice invoice(@PathParam("bill") double bill, @PathParam("billType") int billType, @PathParam("user") int user){
         double discount = discountService.getTotalDiscount(bill, billType, user);
